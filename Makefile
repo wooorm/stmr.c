1be444f9925f3728870e0523d3b38af57a30beba
wooorm <tituswormer@gmail.com>
2014-11-10 19:16:09 +0100

    Add test

--- Makefile
+++ Makefile
@@ -2,9 +2,11 @@ PREFIX ?= /usr/local
 
 SRC = stmr.c
 CLI = stmr-cli.c $(SRC)
+TEST = test.c $(SRC)
 
 OBJ_SRC = $(SRC:.c=.o)
 OBJ_CLI = $(CLI:.c=.o)
+OBJ_TEST = $(TEST:.c=.o)
 
 CFLAGS = -D_GNU_SOURCE -std=c99 \
     -Wall -Wno-format-y2k -W -Wstrict-prototypes -Wmissing-prototypes \
@@ -14,6 +16,9 @@ CFLAGS = -D_GNU_SOURCE -std=c99 \
 
 stmr: $(OBJ_CLI)
 	$(CC) $(OBJ_CLI) -o $@
+
+test: $(OBJ_TEST)
+	$(CC) $(OBJ_TEST) -o $@
 
 .SUFFIXES: .c .o
 .c.o:
@@ -26,6 +31,6 @@ uninstall:
 	rm -f $(PREFIX)/bin/stmr
 
 clean:
-	rm -f stmr $(OBJ_CLI)
+	rm -f stmr $(OBJ_CLI) $(OBJ_TEST)
 
 .PHONY: clean install uninstall
---
+++ test.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "stmr.h"
+
+static int hasError = 0;
+
+static void
+assertStem(char *input, char *output) {
+    char *value;
+    char *result;
+    char *fixture;
+
+    value = strdup(input);
+    result = strdup(input);
+    fixture = strdup(output);
+
+    result[stem(result, 0, strlen(result) - 1) + 1] = 0;
+
+    if (strcmp(fixture, result)) {
+        hasError = 1;
+        fprintf(stderr, "\033[31m");
+        fprintf(stderr,
+            "  (✖) For `%s`. Expected `%s`, got `%s`",
+            value, fixture, result
+        );
+        fprintf(stderr, "\033[0m");
+        fprintf(stderr, "\n");
+    }
+
+    free(value);
+    free(fixture);
+    free(result);
+}
+
+int
+main() {
+    FILE *input;
+    FILE *output;
+
+    char *lineIn;
+    char *lineOut;
+    size_t lengthIn;
+    size_t lengthOut;
+
+    lineIn = NULL;
+    lineOut = NULL;
+    lengthIn = 0;
+    lengthOut = 0;
+
+    input = fopen("fixture/input.txt", "r");
+    output = fopen("fixture/output.txt", "r");
+
+    while (
+        getline(&lineIn, &lengthIn, input) != -1 &&
+        getline(&lineOut, &lengthOut, output) != -1
+    ) {
+
+        lineIn[strlen(lineIn) - 1] = 0;
+        lineOut[strlen(lineOut) - 1] = 0;
+
+        assertStem(lineIn, lineOut);
+    }
+
+    if (hasError == 1) {
+        exit(EXIT_FAILURE);
+    }
+}
